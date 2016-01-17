0d958eaed6c4dc36d15b0aea7b81f7c76f99d588
Jakub K <kntrvch@gmail.com>
2016-01-17 02:33:53 +0100

    Add tournament database schema

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,9 +1,12 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+CREATE DATABASE tournament;
 
+CREATE TABLE players ( id SERIAL,
+                       name VARCHAR,
+                       wins INTEGER,
+                       matches INTEGER );
+
+CREATE TABLE matches ( id SERIAL,
+                       winner INTEGER,
+                       loser INTEGER );
